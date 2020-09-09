0aced778e09d859178f8e64396530670bb0be964
jyotpanwar <jyot_panwar@yahoo.com>
2020-09-09 22:29:04 +0530

    first commit

---
+++ CustomFontDemo/ViewModels/AboutViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using CustomFontDemo.Services;
+using Xamarin.Essentials;
+
+namespace CustomFontDemo.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            //OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://xamarin.com"));
+            OpenWebCommand = new Command(LearnMore);
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        public async void LearnMore()
+        {
+            var dialogService = new DialogService();
+            //var action = new Action(async () => await Browser.OpenAsync("https://xamarin.com"));
+            dialogService.ShowMessage("Alert", "Test Message.", "Ok", null);
+        }
+    }
+}
---
+++ CustomFontDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using CustomFontDemo.Models;
+using CustomFontDemo.ViewModels;
+
+namespace CustomFontDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
